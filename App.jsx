bee1761cb6d5ddc31b4cd34ff7c2e1d841064de4
16mis0500 <68903000+16mis0500@users.noreply.github.com>
2021-06-27 21:02:03 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import Note1 from "./Note1";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+      <Note1 />
+      <Note />
+      <Note1 />
+      <Note />
+    </div>
+  );
+}
+export default App;
---
+++ Note1.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note1() {
+  return (
+    <div className="note">
+      <h1>Javascript and Node.js</h1>
+      <p>
+        This was an amazing bootcamp taken up by Shaurya Sinha.We covered
+        everything from scratch including Javascript and Backend Web Dev with
+        Node.js.
+      </p>
+    </div>
+  );
+}
+export default Note1;
